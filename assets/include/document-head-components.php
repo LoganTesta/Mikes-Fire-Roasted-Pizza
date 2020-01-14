ae9e0b3de609b20f5ac7776a7683c38e882f5aed
logan <logan.testa@outlook.com>
2020-01-14 12:16:18 -0800

    Reorder external css/js

--- assets/include/document-head-components.php
+++ assets/include/document-head-components.php
@@ -1,4 +1,7 @@
 
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
 <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
 <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>     <!-- Babel compiler: required for writing JSX and ES6 JavaScript and translating them to older browsers. -->
@@ -7,8 +10,6 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
  <![endif]-->
 <link rel="icon" type="image/png" href="assets/images/favicon.png" />
-<link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 <link rel="stylesheet" type="text/css" href="assets/css/main-styles-min.css?mod=01142020V2" />
 <link rel="stylesheet" type="text/css" href="assets/css/print-styles-min.css?mod=01142019V2" media="print" />
