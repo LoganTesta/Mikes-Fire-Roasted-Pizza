aa472e0df8f0f32ff84417a7567adcb4bf9399b1
logan <logan.testa@outlook.com>
2020-01-20 12:27:05 -0800

    Improve header image, placement, update index page

--- index.php
+++ index.php
@@ -17,33 +17,37 @@ session_start();
         <div class="body-wrapper">
             <?php include 'assets/include/navigation-content.php'; ?>
             <header class="header">
-                <?php include 'assets/include/header-content.php'; ?>
-                <div class="subtitle-container">
-                    <h2 class="subtitle-container__sub-title">Fresh Custom Pizza For the Whole Party!</h2>
+                <div class="inner-wrapper">
+                    <?php include 'assets/include/header-content.php'; ?>
+                    <div class="subtitle-container">
+                        <h2 class="subtitle-container__sub-title">Fresh Custom Pizza For the Whole Party!</h2>
+                    </div>
                 </div>
             </header>
             <div class="content">
-                <div class="content-row">
-                    <div class="col-sma-6">
-                        <div class="content__text">
-                            <h3>Affordable and Tasty Pizzas</h3>
-                            <p>Mike's Fire-Roasted Pizza makes quality and affordable pizzas.  We make every kind of pizza, from
-                                small personal pizzas to a classic large pizza to enough pizzas
-                                for an entire party.  We are one of the top pizza shops in the Portland area for catering, call us 
-                                at 503-999-9999 or <a href="contact-us.php">contact us</a> at least 3 days in advance.</p>
-                        </div>
-                        <?php include 'assets/include/pizza-info-content.php'; ?>
-                        <div class="content-row">
-                            <div class="col-lar-6">
-                                <div class="content__content-image--small index-one"></div>
+                <div class="inner-wrapper">
+                    <div class="content-row">
+                        <div class="col-sma-6">
+                            <div class="content__text">
+                                <h3>Affordable and Tasty Pizzas</h3>
+                                <p>Mike's Fire-Roasted Pizza makes quality and affordable pizzas.  We make every kind of pizza, from
+                                    small personal pizzas to a classic large pizza to enough pizzas
+                                    for an entire party.  We are one of the top pizza shops in the Portland area for catering, call us 
+                                    at 503-999-9999 or <a href="contact-us.php">contact us</a> at least 3 days in advance.</p>
                             </div>
-                            <div class="col-lar-6">
-                                <div class="content__content-image--small index-two"></div>
+                            <?php include 'assets/include/pizza-info-content.php'; ?>
+                            <div class="content-row">
+                                <div class="col-lar-6">
+                                    <div class="content__content-image--small index-one"></div>
+                                </div>
+                                <div class="col-lar-6">
+                                    <div class="content__content-image--small index-two"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-sma-6">
-                        <div class="pizza-order-content"></div>
+                        <div class="col-sma-6">
+                            <div class="pizza-order-content"></div>
+                        </div>
                     </div>
                 </div>
             </div>
