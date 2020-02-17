db348a8c61464bb5f957c4f1229d03ce9b2db140
logan <logan.testa@outlook.com>
2020-02-17 12:37:22 -0800

    Add gulp-cssnano, gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+var gulp = require('gulp');
+var csssnano = require('gulp-cssnano');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', function () {
+
+});
+
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
+
+var original = '/assets/images/nonmified';
+var result = '/assets/images';
+
+function compressImages() {
+    const output = result;
+
+    return gulp.src(original + "/**/*")
+        .pipe(newer(output))
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest(out));
+};
+exports.images = compressImages;
